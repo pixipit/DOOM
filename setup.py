ad5003076289a043d5cb6ac154a231d145227c27
pixipit <61790959+pixipit@users.noreply.github.com>
2022-10-25 12:52:01 +0200

    windows build packages included

--- setup.py
+++ setup.py
@@ -2,7 +2,23 @@ import platform
 if platform.system() == 'Windows':
     from distutils.core import setup
     import py2exe
-    setup(console=['hello.py'])
+
+    data_files = []
+    setup(
+        name='School',
+        console=['main.py'],
+        options={
+            'py2exe': {
+                'packages': ["sprites"],
+                'dist_dir': 'dist_win',
+                'compressed': True,
+                'includes': [],
+            }
+        },
+
+        data_files=data_files
+
+    )
 else:
     from setuptools import setup
     setup(
