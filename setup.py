89b0a57a3b7f22035450907a499f18ec4672291d
pixipit <61790959+pixipit@users.noreply.github.com>
2022-10-25 12:40:09 +0200

    setup windows support

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
-from setuptools import setup
-setup(
-    app=["main.py"],
-    setup_requires=["py2app"],
-)
+import platform
+if platform.system() == 'Windows':
+    from distutils.core import setup
+    import py2exe
+    setup(console=['hello.py'])
+else:
+    from setuptools import setup
+    setup(
+        app=["main.py"],
+        setup_requires=["py2app"],
+    )
